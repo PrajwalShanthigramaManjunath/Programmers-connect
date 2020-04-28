9bfde3bb9b59c2b5dc345b1ac7203c3ad4a7aced
Prajwal <prajwal.shanthigrama@gmail.com>
2020-04-28 03:00:32 +0100

    get logged in user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { auth, validationResult } = require('express-validator/check');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-//@route GET api/profile
-//@desc Test Route
-//@access Public
+//@route GET api/profile/me
+//@desc Get current user profile
+//@access Private
 
-router.get('/', (req, res) => res.send('Profile Route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+//@route POST api/profile
+//@desc  Create or update user profile
+//@access Private
 
 module.exports = router;
