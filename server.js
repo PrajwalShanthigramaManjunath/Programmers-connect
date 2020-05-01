9d8c1dd06a18831e57aaca8c56bf7dab6179bf05
Prajwal <prajwal.shanthigrama@gmail.com>
2020-05-01 07:05:24 +0100

    Heroku10

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 //To server static assets in production (for heroku deplyoment)
-if (process.env.Node_ENV === 'production') {
-  //Setting static folder
-  app.use(express.static('client/build'));
+// if (process.env.Node_ENV === 'production') {
+//Setting static folder
+app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server started on port ${PORT}'));
