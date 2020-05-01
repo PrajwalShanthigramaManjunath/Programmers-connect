6a7aa9c22f0df8f1c5e0481d8bb3489d48ddc907
Prajwal <prajwal.shanthigrama@gmail.com>
2020-05-01 06:48:44 +0100

    Heroku6

---
+++ client/build/precache-manifest.fd7d2a02bb3e68462983559e8e04334d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005fc8925fcd1d5883182908c32490ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb9419cb31e3b8800f72",
+    "url": "/static/css/main.43aba3df.chunk.css"
+  },
+  {
+    "revision": "26c895f3f796447486bd",
+    "url": "/static/js/2.b9c14420.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.b9c14420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9419cb31e3b8800f72",
+    "url": "/static/js/main.66ecf4a3.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  },
+  {
+    "revision": "5baf22cf8820a789b3401863c0e93b99",
+    "url": "/static/media/showcase.5baf22cf.jpg"
+  }
+]);
