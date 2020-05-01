ce15e9f5c6628ab375fbe3460179550dc54eb74d
Prajwal <prajwal.shanthigrama@gmail.com>
2020-05-01 04:10:13 +0100

    Removed errors for heroku deployment

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -46,7 +46,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i className='fas fa-code' /> Programmers Connect
+          <i className='fad fa-project-diagram' /> Programmers Connect
         </Link>
       </h1>
       {!loading && (
