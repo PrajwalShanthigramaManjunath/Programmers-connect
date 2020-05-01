e4a9f9f2c805d735f6364bdb12cf7b74387ce09d
Prajwal <prajwal.shanthigrama@gmail.com>
2020-05-01 01:41:32 +0100

    client

---
+++ client/stories/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import { MemoryRouter } from 'react-router';
+import Login from '../src/components/auth/Login';
+
+storiesOf('Home Page/auth', module)
+  .addDecorator((story) => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+  .add('default', () => (
+    <Login
+      action={(login) => (
+        <button className='btn w-100 btn-primary'>Test</button>
+      )}
+    />
+  ))
+  .add('exception', () => {
+    return (
+      <Login
+        action={(login) => (
+          <button className='btn w-100 btn-primary'>Test</button>
+        )}
+      />
+    );
+  });
